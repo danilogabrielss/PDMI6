6c898aba2217e8274ce89e30af42350151e2cba5
Danilo Gabriel <danilogabrielss15@gmail.com>
2022-09-30 18:12:40 -0300

    TP02 Finalizado

---
+++ TP02/TP02/TP02/Views/ListView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP02.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListView : ContentPage
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private async void VerProduto(object sender, EventArgs e)
+        {
+            await DisplayAlert("Produto Inserido!", "Produto \nID: " + ID.Text +
+                "\nDescricao: " + Descricao.Text +
+                "\nCategoria: " + Categoria.Text +
+                "\nQuantidade: "+ Quantidade.Text+
+                "\nPreco: " + Preco.Text, "OK");
+        }
+    }
+}
---
+++ TP02/TP02/TP02/Views/TriggersViewBase.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace TP02.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile), XamlFilePath("Views\\TriggersView.xaml")]
+    public class TriggersViewBase
+    {
+
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private void InitializeComponent()
+        {
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TriggersView));
+        }
+    }
+}
